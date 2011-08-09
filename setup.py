8a4f8384d9125929acf141aedb31c95562f7987a
Yann Ramin <atrus@stackworks.net>
2011-08-08 17:50:31 -0700

    1.2.2

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 from distutils.core import setup
 
 setup(name='cymbeline',
-      version="1.2.1",
+      version="1.2.2",
       description='Embedded application framework',
       author='Yann Ramin',
-      author_email='atrus@atrustrivalie.org',
+      author_email='atrus@stackworks.net',
       url='http://www.stackworks.net/cymbeline/',
+      scripts=['cymbelined'],
       packages=['cymbeline'])
